d616f69a644469023afbc7ea8afe766aea279325
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-05-04 10:32:23 +0600

    add style for  active routing

--- src/Pages/Shared/CustomLink/CustomLink.jsx
+++ src/Pages/Shared/CustomLink/CustomLink.jsx
@@ -5,9 +5,18 @@ const CustomLink = ({ children, to, ...props }) => {
   let resolved = useResolvedPath(to);
   let match = useMatch({ path: resolved.pathname, end: true });
 
+  const active = {
+    backgroundColor: "#a0c3d2",
+    color: "black",
+    padding: "6px 14px",
+    fontWeight: "500",
+    borderRadius: "15px 90px 90px 15px",
+    boxShadow: "1px 2px 8px gray",
+  };
+
   return (
     <div>
-      <Link to={to} {...props}>
+      <Link style={match && active} to={to} {...props}>
         {children}
       </Link>
     </div>
