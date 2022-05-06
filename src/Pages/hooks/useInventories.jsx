2f70ac85b960329615a22de293c8951b5c811c10
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-05-06 20:13:17 +0600

    change return value object to array

--- src/Pages/hooks/useInventories.jsx
+++ src/Pages/hooks/useInventories.jsx
@@ -13,7 +13,7 @@ const defaultValue = "https://the-treasure-chest.herokuapp.com/inventory";
 const useInventories = (url) => {
   const { data, error } = useSWR(url ? url : defaultValue, fetcher);
 
-  return { data, isError: error, isLoading: !error && !data };
+  return [data, error, !error && !data];
 };
 
 export default useInventories;
