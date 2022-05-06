1366e56699fbff74bea5087ff03d240a0d9508ac
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-05-06 16:50:14 +0600

    implement useInventories hook

---
+++ src/Pages/hooks/useInventories.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+import useSWR from "swr";
+
+const config = {
+  headers: {
+    auth: `${localStorage.getItem("token")}`,
+  },
+};
+const fetcher = async (...args) =>
+  await axios.get(...args, config).then((res) => res.data);
+const defaultValue = "https://the-treasure-chest.herokuapp.com/inventory";
+
+const useInventories = (url) => {
+  const { data, error } = useSWR(url ? url : defaultValue, fetcher);
+
+  return { data, isError: error, isLoading: !error && !data };
+};
+
+export default useInventories;
