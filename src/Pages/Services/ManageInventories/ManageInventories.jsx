315ce96fe7303d7c47574fdfcbbb1f05c923d6f6
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-05-08 20:55:14 +0600

    add PaginatedItems component

--- src/Pages/Services/ManageInventories/ManageInventories.jsx
+++ src/Pages/Services/ManageInventories/ManageInventories.jsx
@@ -1,45 +1,26 @@
 import React, { useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import useInventories from "../../hooks/useInventories";
-import Spinner from "../../Shared/Spinner/Spinner";
 import AddInventory from "../AddInventory/AddInventory";
-import Inventories from "../Inventories/Inventories";
-import { signOut } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-import auth from "../../../Firebase.init";
+import PaginatedItems from "../PaginatedItems/PaginatedItems";
 import DynamicTitle from "../../Shared/DynamicTitle/DynamicTitle";
 
 const ManageInventories = () => {
-  let [items, isError, isLoading] = useInventories();
-
   const [activeForManage, setActiveForManage] = useState(true);
   const [activeForAdd, setActiveForAdd] = useState(false);
-  const navigate = useNavigate();
-
-  if (isLoading) return <Spinner />;
-  const error = isError;
-
-  if (error?.message?.includes("403") || error?.message?.includes("401")) {
-    signOut(auth);
-    navigate("/signin");
-  }
 
   return (
     <div className="md:max-w-7xl md:mx-auto md:mt-12">
       <DynamicTitle title={"Manage Inventories"} />
-      {error ? (
-        <p className=" text-center text-red-700">{error.message}</p>
-      ) : (
-        <>
-          <div className="flex items-center justify-center">
-            <div className="inline-flex shadow-md hover:shadow-lg focus:shadow-lg">
-              <Link
-                to="/manageinventories"
-                onClick={() => {
-                  setActiveForManage(true);
-                  setActiveForAdd(false);
-                }}
-                className={`
+
+      <div className="flex items-center justify-center">
+        <div className="inline-flex shadow-md hover:shadow-lg focus:shadow-lg">
+          <Link
+            to="/manageinventories"
+            onClick={() => {
+              setActiveForManage(true);
+              setActiveForAdd(false);
+            }}
+            className={`
         rounded-l
         px-6
         py-2.5
@@ -56,16 +37,16 @@ const ManageInventories = () => {
          ${activeForManage ? "  bg-blue-900 " : "  bg-blue-600 "}
          ${activeForManage ? "   font-semibold" : " font-medium"}
       `}
-              >
-                Manage Inventories
-              </Link>
-              <Link
-                onClick={() => {
-                  setActiveForManage(false);
-                  setActiveForAdd(true);
-                }}
-                to="addinvevtory"
-                className={`
+          >
+            Manage Inventories
+          </Link>
+          <Link
+            onClick={() => {
+              setActiveForManage(false);
+              setActiveForAdd(true);
+            }}
+            to="addinvevtory"
+            className={`
         px-6
         py-2.5
         text-white
@@ -81,21 +62,16 @@ const ManageInventories = () => {
         ${activeForAdd ? "  bg-blue-900   " : "  bg-blue-600   "}
           ${activeForManage ? "   font-semibold" : " font-medium"}
       `}
-              >
-                Add Inventory
-              </Link>
-            </div>
-          </div>
-          <Routes>
-            <Route path="/" element={<Inventories items={items} />} />
-            {/* <Route
-              path="displayinvevtory"
-              element={<Displayinvevtory items={items} />}
-            /> */}
-            <Route path="addinvevtory" element={<AddInventory />} />
-          </Routes>
-        </>
-      )}
+          >
+            Add Inventory
+          </Link>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/" element={<PaginatedItems />} />
+
+        <Route path="addinvevtory" element={<AddInventory />} />
+      </Routes>
     </div>
   );
 };
