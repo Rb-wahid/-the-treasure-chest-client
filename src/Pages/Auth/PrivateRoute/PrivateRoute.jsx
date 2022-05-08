04dc7b5335a293c3fb2499cb3df7cda459e2d9d8
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-05-08 13:29:06 +0600

    handle email verification

--- src/Pages/Auth/PrivateRoute/PrivateRoute.jsx
+++ src/Pages/Auth/PrivateRoute/PrivateRoute.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
+import auth from "../../../Firebase.init";
 import useUserEmail from "../../hooks/useUserEmail";
+import Spinner from "../../Shared/Spinner/Spinner";
+import EmailVerification from "../EmailVerification/EmailVerification";
 
 const PrivateRoute = ({ children }) => {
-//  const [user, loadingUser] = useAuthState(auth);
-  const [email, loadingUser] = useUserEmail()
+  const [user, loading] = useAuthState(auth);
+  const [email, loadingUser] = useUserEmail();
   let location = useLocation();
 
   if (!email && !loadingUser) {
     return <Navigate to="/signin" state={{ from: location }} replace />;
+  }
+  if (loading || loadingUser) {
+    return <Spinner />;
+  }
+  console.log(user?.emailVerified);
+  if (!user?.emailVerified) {
+    return <EmailVerification email={email} />;
   }
 
   return children;
